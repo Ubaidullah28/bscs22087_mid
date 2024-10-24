a34f8c991f910c14b4036f8536c36a6d001292ff
Ubaidullah28 <bscs22087@itu.edu.pk>
2024-10-24 10:57:34 +0500

    search bar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import {React, useState} from 'react';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import ListingsGrid from './Components/ListingGrid';
+import Search from './Components/SearchBar';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <SearchBar/>
+      
+      
       <section id="about" style={{ height: '30vh', backgroundColor: '#e2e2e2' }}>
         <h1>About Section</h1>
         <h3>The Open Movie Database
-The OMDb API is a RESTful web service to obtain movie information, all content and images on the site
+The Movies Explorer is a RESTful Movies site to obtain movie information, all content and images on the site
  are contributed and maintained by our users.</h3>
      </section>
       <section id="movies" style={{ height: 'auto', backgroundColor: '#f4f4f4' }}>
